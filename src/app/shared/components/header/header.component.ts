a63cbd882bf54d991207130d387178e97d59773a
royzce <roycemontoya77@gmail.com>
2023-01-18 01:49:12 +0800

    update

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,15 +7,5 @@ import { SharedService } from '../../services/shared.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  constructor(){
-
-  }
-  hideCommandBar(){
-    // this.sharedService.emitData();
-    // console.log(this.sharedService)
-    // this.sharedService.emitData()
-  }
-  showCommandBar(){
-    // this.sharedService.emitData()
-  }
+  constructor(){  }
 }
